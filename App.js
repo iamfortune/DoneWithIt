5483e5eed0953b456741126ad78642f87f5f5989
iamfortune <ikechifortune@gmail.com>
2020-05-18 21:01:54 +0100

    added a js config file to remind VS code that its a JS project

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  let x = 1;
   return (
     <View style={styles.container}>
       <Text>Hello Fortune</Text>
