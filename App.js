68f1ccef25954e21d12d7ee21377cc6a490d07ca
iamfortune <ikechifortune@gmail.com>
2020-05-26 22:41:51 +0100

    added files

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { StyleSheet, Text, Button, SafeAreaView, Dimensions, StatusBar,Platform,View } from "react-native";
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 
 export default function App() {
-  console.log(Dimensions.get('screen'));
+  const { portrait } = useDimensions();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: "100%",
-        height: "30%",
-      }}>
-      </View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent:"center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: "relative",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
