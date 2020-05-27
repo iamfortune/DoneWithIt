86b69c90e2ed67593b60a769d6aeb890d55472a5
iamfortune <ikechifortune@gmail.com>
2020-05-27 02:26:28 +0100

    added a new component

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,26 +1,49 @@
 import React from 'react'
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native'
 
 const WelcomeScreen = () => {
     return (
-        <ImageBackground 
+      <ImageBackground
         style={styles.background}
-        source={require('../assets/background.jpg')}
-        >
+        source={require("../assets/background.jpg")}
+      >
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+          <Text>Sell Items You Don't Need</Text>
+        </View>
         <View style={styles.loginButton}></View>
-        </ImageBackground>
-    )
+        <View style={styles.registerButton}></View>
+      </ImageBackground>
+    );
 }
 
 export default WelcomeScreen
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-    },
-    loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: '#fc5c65'
-    }
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: 'center',
+    // bottom: 10,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+ logo: {
+    width: 100,
+    height: 100,
+ },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
 });
+ 
