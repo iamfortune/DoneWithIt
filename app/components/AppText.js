3adabce87abb687538575ace15db16e84e3702f6
iamfortune <ikechifortune@gmail.com>
2020-06-09 21:09:57 +0100

    adding the stylesheet

---
+++ app/components/AppText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native'
+
+export default function AppText({children}) {
+    return
+       <Text>{children}</Text>
+}
+
+const styles = StyleSheet.create({
+    
+})
+
