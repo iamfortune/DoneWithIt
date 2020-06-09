1d9e82eff734f7c6ad5092de09a1b2a3f893fedc
iamfortune <ikechifortune@gmail.com>
2020-06-09 22:25:55 +0100

    adding the stylesheet

---
+++ app/components/AppTest.ios.js
@@ -0,0 +1,21 @@
+ import React from "react";
+ import { Text, StyleSheet, Platform } from "react-native";
+ import {} from "@expo/vector-icons";
+
+ // headin component
+ function AppText({ children }) {
+   return <Text style={styles.text}>{children}</Text>;
+ }
+
+ const styles = StyleSheet.create({
+   text: {
+     color: "tomato",
+       ios: {
+         fontSize: 20,
+         fontFamily: "Avenir",
+       },
+   },
+ });
+
+ export default AppText;
+
