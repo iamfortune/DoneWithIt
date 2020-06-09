b02a91de1641ad41decb7a16237bb931e18e0e2a
iamfortune <ikechifortune@gmail.com>
2020-06-09 23:44:23 +0100

    added code and stuff

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 
-import AppText from './app/components/AppText';
+import AppText from './app/components/AppTest/AppText';
 
 export default function App() {
   return (
