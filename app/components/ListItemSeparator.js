5cf99c2b08a6905d72c1947ee27a7a8cee28ae74
iamfortune <ikechifortune@gmail.com>
2020-06-20 09:38:14 +0100

    cleaned up the listItemSeparator

--- app/components/ListItemSeparator.js
+++ app/components/ListItemSeparator.js
@@ -3,16 +3,14 @@ import { View, StyleSheet } from 'react-native';
 import colors from '../config/colors';
 
 function ListItemSeparator() {
-    return (
-        <View style={{ width: '100%', height: 1, backgroundColor: '#000' }} />
-    )
+    return <View style={styles.separator} />
 }
 
 const styles = StyleSheet.create({
     separator: {
         width:'100%',
         height: 1,
-        backgroundColor:colors.light
+        backgroundColor:colors.light,
     }, 
 })
 
