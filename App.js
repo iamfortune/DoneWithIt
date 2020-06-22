5a7b50154e8daa2bdb2a165001444d0b33a49100
iamfortune <ikechifortune@gmail.com>
2020-06-23 00:28:17 +0100

    added an icon component

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ import Card from './app/components/Card';
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Icon from './app/components/Icon';
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <MessagesScreen />
+  return ( 
+    <Screen>
+    <Icon 
+    name="email" size={90}
+    />
+  </Screen>
+  )
 }
