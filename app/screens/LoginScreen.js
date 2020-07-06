ac4847d27fec4dea6bb892da6fecc203ede6873d
iamfortune <ikechifortune@gmail.com>
2020-07-06 17:30:01 +0100

    installed and imported yup for validation in form

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,47 +1,54 @@
 import React, {useState} from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
 
 
+const validationSchema = Yup.
+
+
 function LoginScreen() {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
     return (
-        <Screen style={styles.container}>
-            <Image
-                style={styles.logo}
-                source={require('../assets/logo-red.png')} />
-            
-            <Formik
-                initialValues={{}}
-            ></Formik>
-            
-            
-            <AppTextInput
-                autoCapitalize="none" 
+      <Screen style={styles.container}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ handleChange, handleSubmit }) => (
+            <>
+              <AppTextInput
+                autoCapitalize="none"
                 autoCorrect={false}
                 icon="email"
                 keyboardType="email-address"
-                onChangeText={text => setEmail(text)}
+                onChangeText={handleChange('email')}
                 placeholder="Email"
                 textContentType="emailAddress"
-            />
-            <AppTextInput
+              />
+              <AppTextInput
                 autoCapitalize="none"
                 autoCorrect={false}
                 icon="lock"
-                onChangeText={text => setPassword(text)}
+                onChangeText={handleChange('password')}
                 placeholder="Password"
                 secureTextEntry={true}
                 textContentType="password"
-            />
-            <AppButton title="Login" onPress={() => console.log(email, password)} />
-        </Screen>
-    )
+              />
+              <AppButton
+                title="Login"
+                onPress={handleSubmit}
+              />
+            </>
+          )}
+        </Formik>
+      </Screen>
+    );
 }
 
 const styles = StyleSheet.create({
