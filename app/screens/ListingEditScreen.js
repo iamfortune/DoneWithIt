64b37c2debeb2347c12448a675f9de6123a2ddd1
iamfortune <ikechifortune@gmail.com>
2020-07-15 14:59:28 +0100

    fixing the width of the listing edit screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -43,6 +43,7 @@ function ListingEditScreen() {
           maxLength={8}
           name="price"
           placeholder="Price"
+          width={120}
         />
         <AppFormPicker
           items={categories}
