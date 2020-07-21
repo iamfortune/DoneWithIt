296580fda96db683451139fbdfade65ec18e5794
iamfortune <ikechifortune@gmail.com>
2020-07-21 19:21:16 +0100

    fixed the scrolling on the image input list component

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
-  return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+    const scrollView = useRef();
+    
+    return (
+        <View>
+        <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
         </View>
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
-    </View>
-  );
+      </ScrollView>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
