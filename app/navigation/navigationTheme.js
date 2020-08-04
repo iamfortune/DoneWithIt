f8e1f3c9a8526eafd33eff56767df8bdbb5ce81a
iamfortune <ikechifortune@gmail.com>
2020-08-04 09:24:10 +0100

    overrided the default native color themes for our project

--- app/navigation/navigationTheme.js
+++ app/navigation/navigationTheme.js
@@ -1,8 +1,11 @@
 import { DefaultTheme } from '@react-navigation/native';
+import colors from '../config/colors';
 
 const myTheme = {
     ...DefaultTheme,
     colors: {
-        
+        ...DefaultTheme.colors,
+        primary: colors.primary,
+        background: colors.white
     }
 }
