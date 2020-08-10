f875b95ca519c6aafced297cbc64bdb890c742c4
iamfortune <ikechifortune@gmail.com>
2020-08-10 16:24:59 +0100

    exported default app navigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import AccountScreen from "../screens/AccountScreen";
+import ListingEditScreen from "../screens/ListingEditScreen";
+import ListingsScreen from '../screens/ListingsScreen';
+
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
-    <Tab.Navigator>
-            
-    </Tab.Navigator>
-)
+  <Tab.Navigator>
+    <Tab.Screen name="Listings" component={ListingsScreen} />
+    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+export default AppNavigator;
