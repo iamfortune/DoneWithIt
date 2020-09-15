2d24f22f3d16177eeb581889e25bba61df4e38c8
iamfortune <ikechifortune@gmail.com>
2020-09-15 12:46:44 +0100

    updating develop

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from "react";
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import { View, Text } from "react-native";
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer, useNavigation } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import Card from './app/components/Card';
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Icon from './app/components/Icon';
-import Screen from "./app/components/Screen";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ListingsScreen from "./app/screens/ListingsScreen";
-import { TextInput } from "react-native";
-import AppTextInput from "./app/components/AppTextInput";
-import { Switch } from "react-native";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import AuthNavigator from "./app/navigation/AuthNavigator";
+import React from "react";
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-
-
 
 
 export default function App() {
@@ -39,12 +13,3 @@ export default function App() {
   );
 }
 
-
-
-// The Great Gatsby == the part in Daisy's dock in the book
-// it has a quote, and it says...
-// He had come a long way to this blue lawn, 
-// and his dreams must have seemed so close,
-// that he could hardly fail to grasp it...
-
-// Inside Bill's head 
